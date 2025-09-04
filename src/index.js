adedb4f71b7b661ae97ca4aed31ea898613ff01c
id-edm <ivandubochit@gmail.com>
2025-09-04 18:37:50 +0400

    feat: ready task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix) {
+  if (!matrix || matrix.length === 0) {
+    return [];
+  }
+
+  const result = [];
+
+  for (let i = 0; i < matrix.length; i += 1) {
+    if (i % 2 === 0) {
+      result.push(...matrix[i]);
+    } else {
+      result.push(...matrix[i].reverse());
+    }
+  }
+
+  return result;
 };
